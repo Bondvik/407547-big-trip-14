bd37b20e7410cbcd3f6b5c3953870d3ddcfb77c7
Viktoriia <bondvik16@gmail.com>
2021-04-27 18:37:10 +0300

    Test сортировки

--- src/mock/util.js
+++ src/mock/util.js
@@ -1,3 +1,11 @@
+import dayjs from 'dayjs';
+
+const SortType = {
+  DEFAULT: 'default',
+  TIME: 'time',
+  PRICE: 'price',
+};
+
 //Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 const getRandomNumber = function(min, max) {
   let minValue =  Math.ceil(min);
@@ -23,4 +31,34 @@ const updateItem = (items, updatedPoint) => {
   return items;
 };
 
-export {getRandomNumber, updateItem};
+const compareEventPrice = (prevEvent, nextEvent) => nextEvent.eventTotal - prevEvent.eventTotal;
+
+// Функция помещает задачи без даты в конце списка,
+// возвращая нужный вес для колбэка sort
+const getWeightForNullDate = (dateA, dateB) => {
+  if (dateA === null && dateB === null) {
+    return 0;
+  }
+
+  if (dateA === null) {
+    return 1;
+  }
+
+  if (dateB === null) {
+    return -1;
+  }
+
+  return null;
+};
+
+const sortEventDown = (prevEvent, nextEvent) => {
+  const weight = getWeightForNullDate(prevEvent.eventDuration, nextEvent.eventDuration);
+
+  if (weight !== null) {
+    return weight;
+  }
+  console.log(dayjs(nextEvent.eventDuration).diff(dayjs(prevEvent.eventDuration)))
+  return dayjs(nextEvent.eventDuration).diff(dayjs(prevEvent.eventDuration));
+};
+
+export {getRandomNumber, updateItem, SortType, sortEventDown, compareEventPrice};
